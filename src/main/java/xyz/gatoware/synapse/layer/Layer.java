962b08f2f39369d409617d34c433edba0dfd1600
Gatoware <110510127+Gatoware-6310@users.noreply.github.com>
2026-08-18 14:57:11 -0400

    Wire optimizers into layer API

--- src/main/java/xyz/gatoware/synapse/layer/Layer.java
+++ src/main/java/xyz/gatoware/synapse/layer/Layer.java
@@ -1,6 +1,7 @@
 package xyz.gatoware.synapse.layer;
 
 import xyz.gatoware.synapse.matrix.Matrix;
+import xyz.gatoware.synapse.optimizer.Optimizer;
 
 /** A layer in a neural network. */
 public interface Layer {
@@ -17,5 +18,16 @@ public interface Layer {
 	 */
 	default Matrix backward(Matrix outputGradient, float learningRate) {
 		throw new UnsupportedOperationException("Layer does not support training");
+	}
+
+	/** Runs backpropagation using the given optimizer and returns the input gradient.
+	 * Layers that do not override this method fall back to their existing backward implementation.
+	 * @param outputGradient the gradient at the layer output
+	 * @param learningRate the training learning rate
+	 * @param optimizer the optimizer used to update trainable parameters
+	 * @return the gradient at the layer input
+	 */
+	default Matrix backward(Matrix outputGradient, float learningRate, Optimizer optimizer) {
+		return backward(outputGradient, learningRate);
 	}
 }
