3b99570df975b215504e62ccf60fe77074a738c6
Gatoware <110510127+Gatoware-6310@users.noreply.github.com>
2026-08-22 22:34:23 -0400

    Rename VALID padding to NONE

--- src/main/java/xyz/gatoware/synapse/layer/Padding.java
+++ src/main/java/xyz/gatoware/synapse/layer/Padding.java
@@ -3,7 +3,11 @@ package xyz.gatoware.synapse.layer;
 /** Padding modes for convolution layers. */
 public enum Padding {
 	/** Do not pad the input. */
-	VALID,
+	NONE,
 	/** Pad the input so spatial output size is preserved for stride 1. */
-	SAME
+	SAME;
+
+	/** @deprecated Use {@link #NONE}. */
+	@Deprecated
+	public static final Padding VALID = NONE;
 }
